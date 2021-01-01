2901847ca22bca5bf27f3006e878d4fcaaf20df0
parapi1204 <59352390+parapi1204@users.noreply.github.com>
2021-01-01 21:53:21 +0900

    AND, NAND, OR

--- dl_functions.py
+++ dl_functions.py
@@ -1,8 +1,34 @@
+import numpy as np
+
 
 def AND(x1: int, x2: int) -> int:
-    w1, w2, theta = 0.5, 0.5, 0.7
-    tmp = x1*w1 + x2*w2
-    if tmp <= theta:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
         return 0
-    elif tmp > theta:
+    else:
+        return 1
+
+
+def NAND(x1: int, x2: int) -> int:
+    x = np.array([x1, x2])
+    w = np.array([-0.5, -0.5])
+    b = 0.7
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
+        return 1
+
+
+def OR(x1: int, x2: int) -> int:
+    x = np.array([x1, x2])
+    w = np.array([0.5, 0.5])
+    b = -0.2
+    tmp = np.sum(w*x) + b
+    if tmp <= 0:
+        return 0
+    else:
         return 1
