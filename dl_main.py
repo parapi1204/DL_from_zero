84fc594f4b6991dad49120e03fa2884a75386a14
parapi1204 <59352390+parapi1204@users.noreply.github.com>
2021-04-02 07:27:34 +0900

    Implement TwoLayerNet class.

--- dl_main.py
+++ dl_main.py
@@ -1,21 +1,37 @@
 import numpy as np
+from mnist import load_mnist
+import matplotlib.pyplot as plt
 import dl_functions as dlf
 
-# x, t = dlf.get_data()
-# network = dlf.init_network_mnist()
-
 (x_train, t_train), (x_test, t_test) = \
-    dlf.load_mnist(normalize=True, one_hot_label=True)
+    load_mnist(normalize=True, flatten=True, one_hot_label=True)
 
+train_loss_list = []
+
+# hyper parameter
+iters_num = 10000
 train_size = x_train.shape[0]
-batch_size = 10
-batch_mask = np.random.choice(train_size, batch_size)
-x_batch = x_train[batch_mask]
-t_batch = t_train[batch_mask]
+batch_size = 100
+learning_rate = 0.1
 
-print(x_batch.size)
-print(x_batch.shape)
+network = dlf.TwoLayerNet(input_size=784, hidden_size=50, output_size=10)
 
-print(dlf.numerical_gradient(dlf.function_2, np.array([3.0, 4.0])))
-print(dlf.numerical_gradient(dlf.function_2, np.array([0.0, 2.0])))
-print(dlf.numerical_gradient(dlf.function_2, np.array([3.0, 0.0])))
+for i in range(iters_num):
+    batch_mask = np.random.choice(train_size, batch_size)
+    x_batch = x_train[batch_mask]
+    t_batch = t_train[batch_mask]
+
+    grad = network.numerical_gradient(x_batch, t_batch)
+    # grad = network.gradient(x_batch, t_batch)
+
+    for key in ['W1', 'b1', 'W2', 'b2']:
+        network.params[key] -= learning_rate * grad[key]
+
+    loss = network.loss(x_batch, t_batch)
+    train_loss_list.append(loss)
+    print("epoch {} is finished. loss = {}".format(i, loss))
+
+fig = plt.figure()
+ax = fig.add_subplot()
+ax.plot(range(iters_num), train_loss_list)
+fig.show()
