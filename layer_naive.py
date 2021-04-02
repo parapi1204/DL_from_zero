9765bf1592be837dc1cc0a85523f4d1116b1da3e
tai <tai_t@hitachizosen.co.jp>
2021-04-02 18:14:33 +0900

    Affine, ReLU, Sigmoid

---
+++ layer_naive.py
@@ -0,0 +1,29 @@
+class MulLayer:
+    def __init__(self):
+        self.x = None
+        self.y = None
+
+    def forward(self, x, y):
+        self.x = x
+        self.y = y
+
+        return x * y
+
+    def backward(self, dout):
+        dx = dout * self.y
+        dy = dout * self.x
+
+        return dx, dy
+
+
+class AddLayer:
+    def _init__(self):
+        pass
+
+    def forward(self, x, y):
+        return x + y
+
+    def backward(self, dout):
+        dx = dout
+        dy = dout
+        return dx, dy
---
+++ layers.py
@@ -0,0 +1,45 @@
+import numpy as np
+
+
+class ReLU:
+    def __init__(self):
+        self.mask = None
+
+    def forward(self, x):
+        self.mask = (x <= 0)
+        out = x.copy()
+        out[self.mask] = 0
+        return out
+
+
+class Sigmoid:
+    def __init__(self):
+        self.out = None
+
+    def forward(self, x):
+        out = 1 / (1 + np.exp(-x))
+        self.out = out
+        return out
+
+    def backward(self, dout):
+        return dout * self.out * (1 - self.out)
+
+
+class Affine:
+    def __init__(self, W, b):
+        self.W = W
+        self.b = b
+        self.x = None
+        self.dW = None
+        self.db = None
+
+    def forward(self, x):
+        self.x = x
+        out = np.dot(x, self.W) + self.b
+        return out
+
+    def backward(self, dout):
+        dx = np.dot(dout, self.W.T)
+        self.dW = np.dot(self.x.T, dout)
+        self.db = np.sum(dout, axis=0)
+        return dx
